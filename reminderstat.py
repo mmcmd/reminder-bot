7d2237775823d15a0cbf4d9c2a2288170633b07e
tam duong <tduong10101@gmail.com>
2019-10-16 20:01:33 +1100

    add get_config function and updated script to read from json data

--- reminderstat.py
+++ reminderstat.py
@@ -1,14 +1,39 @@
 import discord
+import os
+import sys
+import json
+
+
+def get_config():
+    # grab ./config.json if it is existed
+    config = None
+    if os.path.exists('config.json'):
+        # load config file
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+    return config
+
+
+CONFIG = get_config()
+if not CONFIG:
+    message = "Please create a config.json config file in the same directory with remiderstat.py\n" \
+              "Below is an example of the config.json file.\n"\
+              "{\n" \
+              "\t\"token\":\"<token>\",\n" \
+              "\t\"channel\":\"<channel>\",\n" \
+              "\t\"message\":\"<message>\"\n" \
+              "}"
+    print(message)
+    sys.exit()
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-    reminderchannel = client.get_channel(573214438086410240)
-    await reminderchannel.send("Hey, it's the first of the month. Monthly reminder to type `s?t` in the <#%s> channel. <@%s>"% (362848533159870474, 109734116034940928))
+    reminderchannel = client.get_channel(int(CONFIG['channel']))
+    await reminderchannel.send(CONFIG['message'])
     await client.close()
 
-
-
-client.run('TOKEN')
+client.run(CONFIG['token'])
